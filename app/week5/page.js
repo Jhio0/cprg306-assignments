dc147814400fd94db8e7830aabf03c2cfd566c25
Jhio0 <120062814+Jhio0@users.noreply.github.com>
2023-10-04 10:06:32 -0600

    week 5 finish

--- app/week5/page.js
+++ app/week5/page.js
@@ -18,8 +18,10 @@ function generate(props) {
 function page() {
 
     const [sortBy, setSortBy] = useState("name");
+    const [groupedByCategory, setGroupedByCategory] = useState(false);
     
     const sortedItems = [...itemlist];
+    
 
     if (sortBy === 'name') {
         sortedItems.sort((a, b) => a.name.localeCompare(b.name));
@@ -27,28 +29,60 @@ function page() {
         sortedItems.sort((a, b) => a.category.localeCompare(b.category));
     }
 
+    const groupedItems = sortedItems.reduce((result, item) => {
+        if (!result[item.category]) {
+            result[item.category] = [];
+        }
+        result[item.category].push(item);
+        return result;
+    }, {})
+    //sorted by category
+    const sortedCategories = Object.keys(groupedItems).sort();
+
     const handleSortByChange = (value) => {
         setSortBy(value);
+    };
+
+    const handleGroupByCategory = () => {
+        setGroupedByCategory(!groupedByCategory);
     };
 
 
     return (
     <main class="bg-slate-950">
         <div class="text-xl font-bold">
+            <div>Sort By: </div>
             <div>
             <button
                 onClick={() => handleSortByChange('name')}
-                style={{ backgroundColor: sortBy === 'name' ? 'lightblue' : 'white' }}>
-                Sort by Name
+                style={{ backgroundColor: sortBy === 'name' ? 'lightblue' : 'white' }}
+                className="text-black">
+                Name
             </button>
             </div>
             <button
                 onClick={() => handleSortByChange('category')}
-                style={{ backgroundColor: sortBy === 'category' ? 'lightblue' : 'white' }}>
-                Sort by category
+                style={{ backgroundColor: sortBy === 'category' ? 'lightblue' : 'white' }}
+                className="text-black">
+                category
             </button>
-            <h2>Shopping List</h2>
-            {sortedItems.map(generate)}
+            <div>
+                <button 
+                    onClick={handleGroupByCategory}
+                    style={{ backgroundColor: groupedByCategory ? 'lightblue' : 'white' }}
+                    className="text-black">
+                    {groupedByCategory ? "Ungroup" : "Group by Category"}
+                </button>
+            </div>
+                <h2>Shopping List</h2>
+                {groupedByCategory
+                    ? sortedCategories.map((category) => (
+                        <div key={category}>
+                            <h3 className="capitalize">{category}</h3>
+                            {groupedItems[category].map(generate)}
+                        </div>
+                    ))
+                :sortedItems.map(generate)}
         </div>
     </main>
     ) 
